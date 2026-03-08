339d4c708aca0607aa945bf5120fc34deeea80de
timka304 <304timka@gmail.com>
2026-03-08 15:37:26 +0000

    Add files via upload

--- lesson3.py
+++ lesson3.py
@@ -45,3 +45,59 @@ bins = [10, 20, 30, 40, 50]
 
 plt.hist(int1, bins)
 plt.show()
+
+
+
+#HISTOGRAM
+
+random_numbers = np.random.randint(1, 50, 100)
+
+intervals = [0, 10, 20, 30, 40, 50]
+
+plt.hist(random_numbers, bins= intervals)
+plt.show()
+
+
+#SCATTER PLOT
+
+number_sold = np.random.randint(1, 20, 6)
+temperature = [0, 1, 2, 3, 4, 5]
+
+
+plt.scatter(number_sold, temperature)
+
+plt.show()
+
+
+#STACK
+
+activities = ["football", "running", "basketball", "cricket"]
+monday_hours = [2, 1, 2, 3]
+tuesday_hours = [3, 2, 4, 3]
+wednesday_hours = [1, 1, 2, 1]
+thursday_hours = [4, 3, 1, 2]
+
+plt.stackplot(activities, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, labels= ["Monday", "Tuesday", "Wednesday", "Thursday"])
+
+plt.legend()
+
+plt.xlabel("Activities")
+plt.ylabel("Hours Spent")
+
+
+plt.show()
+
+
+#SUB PLOT
+
+plt.figure()
+
+plt.subplot(121)
+
+plt.stackplot(activities, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, labels= ["Monday", "Tuesday", "Wednesday", "Thursday"])
+
+
+plt.subplot(122)
+
+plt.scatter(number_sold, temperature)
+plt.show()
